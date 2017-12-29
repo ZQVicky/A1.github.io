dac821d38d2b75a74e82ed4f8b080f04e062bbb8
ZQVicky <32510491+ZQVicky@users.noreply.github.com>
2017-12-29 19:13:14 +0000

    Update search.js

--- search.js
+++ search.js
@@ -1,36 +1,46 @@
 const API_KEY = "c04c6f3a6b250f5dd4a8f2864e0620d5";
 const APP_ID = "ff6e13d9"
 
-
+//function for call the api's information and diaplay them in the results section.
 var parseResponse = function() {
     var response = JSON.parse(this.response);
     console.log(response);
     var results = response.matches;
-    for(var i = 0; i < 10; i++) {
-      console.log(results[i]);
-      var namePrint = document.createElement('h4');
-      namePrint.append(document.createTextNode(results[i].recipeName));
-      var timePrint = document.createElement('p');
-      var totaltime = results[i].totalTimeInSeconds/60;
-      timePrint.append(document.createTextNode('Total Time: ' + totaltime));
-      var ratePrint = document.createElement('p');
-      ratePrint.append(document.createTextNode('Rating: '+results[i].rating));
-      var ingredientsPrint = document.createElement('p');
-      ingredientsPrint.append(document.createTextNode('ingredients: '+ results[i].ingredients));
-      var imgNode = document.createElement('img');
-      imgNode.id = 'img_api';
-      imgNode.src = results[i].imageUrlsBySize['90'];
+    if (results == 0){
+      var nullPrint = document.createElement('h1');
+      nullPrint.append("Sorry, we can't find the recipes.\n\n Please search again.");
+      output.append(nullPrint);
+    }else {
+      for(var i = 0; i < 10; i++) {
+        console.log(results[i]);
+        var namePrint = document.createElement('h4');
+        namePrint.append(document.createTextNode(results[i].recipeName));
+        var timePrint = document.createElement('p');
+        var totaltime = results[i].totalTimeInSeconds/60; //calculating second into minutes
+        timePrint.append(document.createTextNode('Total Time: ' + totaltime));
+        var ratePrint = document.createElement('p');
+        ratePrint.append(document.createTextNode('Rating: '+results[i].rating));
+        var ingredientsPrint = document.createElement('p');
+        ingredientsPrint.append(document.createTextNode('ingredients: '+'\n'+results[i].ingredients));
+        var imgNode = document.createElement('img');
+        imgNode.id = 'img_api';
+        imgNode.src = results[i].imageUrlsBySize['90'];
 
-      output.append(namePrint);
-      output.append(imgNode);
-      output.append(timePrint);
-      output.append(ratePrint);
-      output.append(ingredientsPrint);
 
+        var divCreate = document.createElement('div');//creating a div element to hold each results information.
+        divCreate.id = 'description';
+        divCreate.appendChild(namePrint);
+        divCreate.appendChild(imgNode);
+        divCreate.appendChild(imgNode);
+        divCreate.appendChild(timePrint);
+        divCreate.appendChild(ratePrint);
+        divCreate.appendChild(ingredientsPrint);
 
+        output.append(divCreate);
+
+      }
     }
   }
-
 
 // Function to ensure parameters used in request are encoded correctly
 var encodeParameters = function(params) {
@@ -55,10 +65,6 @@ var doSearch = function() {
         _app_id: APP_ID,
         _app_key: API_KEY,
         q:  search_term,
-        // ingr: search_term,
-        // app_id: APP_ID,
-        // app_key: API_KEY,
-        // page: '0',
 
     }
 
@@ -71,18 +77,104 @@ var doSearch = function() {
     xhttp.send();
 }
 
-var btn = document.getElementById('search_button');
-    btn.onclick = function (){
-        location.reload();
-    }
 var search_button = document.getElementById('search_button');
-search_button.addEventListener('click', doSearch);
+search_button.addEventListener('click',doSearch);
+  
+
+// click picture showing BREAKFAST
+var imgClick = function() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.addEventListener('load', parseResponse);
+    var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ff6e13d9&_app_key=c04c6f3a6b250f5dd4a8f2864e0620d5&q=breakfast';
+    console.log(url);
+    xhttp.open('GET', url);
+    xhttp.send();
+}
+
+var img_api = document.getElementById('breakfast');
+img_api.addEventListener('click', imgClick);
+
+// click picture showing LUNCH
+var imgClick = function() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.addEventListener('load', parseResponse);
+    var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ff6e13d9&_app_key=c04c6f3a6b250f5dd4a8f2864e0620d5&q=lunch';
+    console.log(url);
+    xhttp.open('GET', url);
+    xhttp.send();
+}
+
+var img_api = document.getElementById('lunch');
+img_api.addEventListener('click', imgClick);
+
+// click picture showing DINNER
+var imgClick = function() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.addEventListener('load', parseResponse);
+    var url = 'http://api.yummly.com/v1/api/recipes?_app_id=ff6e13d9&_app_key=c04c6f3a6b250f5dd4a8f2864e0620d5&q=dinner';
+    console.log(url);
+    xhttp.open('GET', url);
+    xhttp.send();
+}
+
+var img_api = document.getElementById('dinner');
+img_api.addEventListener('click', imgClick);
+
+//searching videos
+const YOUR_API_KEY = "AIzaSyBqAWageRpzahJDqBvM9IQB65iK9S0nzNs";
+
+var parseRes = function() {
+    var response = JSON.parse(this.response);
+    console.log(response);
+    var results = response.items;
+    for(var i = 0; i < 10; i++) {
+      console.log(results[i]);
+      var titlePrint = document.createElement('h3');
+      titlePrint.append(document.createTextNode(results[i].snippet['title']));
+      var desPrint = document.createElement('p');
+      desPrint.append(document.createTextNode(results[i].snippet.description));
+      // get videoid from results and creat a new link to play the video.
+      var vlog = document.createElement('iframe');
+      vlog.id = 'vlogs';
+      vlog.src = "https://www.youtube.com/embed/" + results[i].id['videoId'];
+      vlog.frameborder= "0"
+
+      var divCreate = document.createElement('div');//creating a div element to hold each results information.
+      divCreate.id = 'videos';
+      divCreate.appendChild(titlePrint);
+      divCreate.appendChild(vlog);
+      divCreate.appendChild(desPrint);
+
+      output.append(divCreate);
+
+    }
+  }
+var Search = function() {
+    var search_video = document.getElementById('search_video').value;
+    console.log(search_video);
+
+    parameters = {
+
+      part:'snippet',
+      maxResults: 25,
+      q: search_video,
+      type: 'video',
+      key:YOUR_API_KEY
+
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.addEventListener('load', parseRes);
+    var url = 'https://www.googleapis.com/youtube/v3/search?'+ encodeParameters(parameters);
+    console.log(url);
+    xhttp.open('GET', url);
+    xhttp.send();
 
 
-// // click picture showing results
-// var doSearch = function() {
-//     var search_term = document.getElementById('search_term').value;
-//     console.log(search_term);
-// var rec = document.getElementById('a');
-// imgNode.id = 'rec_api';
-// imgNode.href = "http://api.yummly.com/v1/api/recipes?_app_id=ff6e13d9&_app_key=c04c6f3a6b250f5dd4a8f2864e0620d5&q=breakfast";
+}
+
+var search_button = document.getElementById('search_button2');
+search_button.addEventListener('click',Search);
